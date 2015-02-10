fcd8dafe6c09fb31f792675a8ea2f96db2cc1881
Eda Riedl <eduardriedl@gmail.com>
2015-02-10 20:50:09 +0100

    Add the rest of user resource.

--- lib/fakturoid/client/user.rb
+++ lib/fakturoid/client/user.rb
@@ -1,8 +1,18 @@
 module Fakturoid
   module Client
     class User < Api
-      def self.get
+      def self.current
         get_request('user.json', url: Fakturoid::Api.config.endpoint_without_account)
+      end
+      
+      def self.find(id)
+        raise ArgumentError, "ID has to be integer" unless id.is_a?(Integer)
+        
+        get_request("users/#{id}.json")
+      end
+      
+      def self.all
+        get_request("users.json")
       end
     end
   end
