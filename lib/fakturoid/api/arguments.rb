e53eafc9bf10170f43136c13ff31addea6e9d2cc
Eda Riedl <eduardriedl@gmail.com>
2015-02-11 23:52:33 +0100

    Improve validations and add tests for them.

--- lib/fakturoid/api/arguments.rb
+++ lib/fakturoid/api/arguments.rb
@@ -6,11 +6,13 @@ module Fakturoid
       end
       
       def validate_id(id)
-        raise ArgumentError, "Wrong ID given: #{id}" unless id.is_a?(Integer)
+        raise ArgumentError, "Wrong ID given: #{id}" if !id.is_a?(Integer) && !(id.is_a?(String) && id =~ /\A\d+\z/)
+        true
       end
       
       def validate_search_query(query)
         raise ArgumentError, "Query parameter is required" if query.nil? || query.empty?
+        true
       end
     end
   end
