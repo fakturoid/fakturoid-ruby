ad4dfa0f1330bde802835cf2021ea790e83a39ad
Eda Riedl <eduardriedl@gmail.com>
2015-02-23 12:43:35 +0100

    Add event and todo resources.

---
+++ lib/fakturoid/client/todo.rb
@@ -0,0 +1,16 @@
+module Fakturoid
+  module Client
+    class Todo < Fakturoid::Api
+      def self.all(params = {})
+        request_params = permit_params(params, :page, :since) || {}
+        
+        get_request('todos.json', request_params: request_params)
+      end
+      
+      def self.toggle_completion(id)
+        validate_numerical_id(id)
+        post_request("todos/#{id}/toggle_completion.json")
+      end
+    end
+  end
+end
