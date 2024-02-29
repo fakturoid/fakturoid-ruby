8b8a5d6b2c0b3b1d7ba4810e3ed715e32b74c484
Oldrich Vetesnik <oldrich.vetesnik@gmail.com>
2024-02-29 16:19:50 +0100

    Revise events.

--- lib/fakturoid/api/event.rb
+++ lib/fakturoid/api/event.rb
@@ -5,14 +5,16 @@ module Fakturoid
     class Event
       include Common::Base
 
+      INDEX_PARAMS = [:page, :since, :subject_id].freeze
+
       def all(params = {})
-        request_params = Utils.permit_params(params, :page, :since, :subject_id) || {}
+        request_params = Utils.permit_params(params, *INDEX_PARAMS) || {}
 
         perform_request(HTTP_GET, "events.json", request_params: request_params)
       end
 
       def paid(params = {})
-        request_params = Utils.permit_params(params, :page, :since, :subject_id) || {}
+        request_params = Utils.permit_params(params, *INDEX_PARAMS) || {}
 
         perform_request(HTTP_GET, "events/paid.json", request_params: request_params)
       end
